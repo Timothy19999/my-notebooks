16bdf38bd9ba8c13d880ecacdc20b9721fd72358
Timothy19999 <htoomyatminbo22@gmail.com>
2025-09-01 23:27:30 +0700

    Initial commit: added notebook

---
+++ app.py
@@ -0,0 +1,15 @@
+hot = False
+cold = False 
+windy = False
+ 
+if hot:
+    print("Take your hat")
+    print("It's a hot day")
+elif cold:
+    print("Wear your jacket")
+    print("It's a cold day")
+elif windy:
+    print("Take care of yourself")
+    print("It is super windy day")
+else:
+    print("It's a lovely day")
